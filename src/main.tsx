a00c4ba87702b5e7d99ab26f95a287fb6431dec4
unknown <syednew5000@gmail.com>
2023-07-15 15:36:40 +0600

    [done] design landing page

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import routes from './routes/routes.tsx';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+
+    <RouterProvider router={routes} />
+
+  </React.StrictMode>
+);
---
+++ src/routes/routes.tsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '@/App';
+import Login from '@/pages/Login';
+import NotFound from '@/pages/NotFound';
+import Home from '@/pages/Home';
+import Signup from '@/pages/Signup';
+
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
+
+export default routes;
